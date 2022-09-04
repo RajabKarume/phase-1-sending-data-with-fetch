d0fb23cf9f07a3bcecd13c7a059887b4b7a9d3ee
Rajab <rkarume@gmail.com>
2022-09-05 00:02:05 +0300

    commit tree c055baac25390f77982ee35725216297b3c30bd7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: "Steve",
+          email: "steve@steve.com",
+        }),
+      })
+    
+};
+fetch("http://localhost:3000/users", submitData)
+    .then((responce) => responce.json())
+    .then((object) => console.log(object))
+    .catch((error) => {
+        alert("Unauthorized Access")
+        console.log(error.message)
+    });
